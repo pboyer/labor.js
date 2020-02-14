93650edbec0c98b73265fffe776c0636336d9a15
Peter Boyer <peter.b.boyer@gmail.com>
2020-02-14 09:56:41 -0500

    Format

--- src/labor.js
+++ src/labor.js
@@ -1,131 +1,118 @@
-if ( typeof exports != 'object' || exports === undefined )  // browser context
-{
-	var labor = window.labor || {};
+if (typeof exports != "object" || exports === undefined) {
+  // browser context
+  var labor = window.labor || {};
+} // node.js context
+else {
+  var labor = (module.exports = {});
 }
-else // node.js context
-{
-	var labor = module.exports = {};
-}
 
-(function(labor) { 
+(function(labor) {
+  // Use a library for Worker if it doesn't exist. This is not done
+  // in the node.js so this library can be used with browserify
+  if (typeof Worker === "undefined") {
+    Worker = require("webworker-threads").Worker;
+  }
 
-	// Use a library for Worker if it doesn't exist. This is not done
-	// in the node.js so this library can be used with browserify
-	if (typeof Worker === 'undefined') {
-	  	Worker = require('webworker-threads').Worker;
-	}
+  labor.Router = function(library) {
+    if (arguments.callee._singletonInstance)
+      return arguments.callee._singletonInstance;
 
-	labor.Router = function( library )
-	{
+    arguments.callee._singletonInstance = this;
 
-		if ( arguments.callee._singletonInstance )
-	    return arguments.callee._singletonInstance;
+    this.lib = library;
 
-	  arguments.callee._singletonInstance = this;
+    var self = this;
 
-		this.lib = library;
+    onmessage = function(e) {
+      postMessage({
+        result: self.lib[e.data.func].apply(null, e.data.arguments),
+        id: e.data.id
+      });
+    };
+  };
 
-		var self = this;
+  labor.Pool = function(filename, count) {
+    this.callbacks = {};
+    this.filename = filename;
+    this.count = count || 4;
+    this.queue = [];
+    this.results = [];
+    this.pool = [];
+    this.working = {};
+    this.uuid = 0;
+    return this;
+  };
 
-		onmessage = function( e ){
-			postMessage( { result: self.lib[ e.data.func ].apply( null, e.data.arguments ), id: e.data.id } );
-		};
+  labor.Pool.prototype.start = function() {
+    this.fillPool();
+    return this;
+  };
 
-	};
+  labor.Pool.prototype.onfinish = function() {};
 
-	labor.Pool = function(filename, count) {
+  labor.Pool.prototype.addWork = function(
+    function_name,
+    arguments_array,
+    callback
+  ) {
+    var id = this.uuid++;
+    this.callbacks[id] = callback;
+    this.queue.push({
+      func: function_name,
+      arguments: arguments_array,
+      id: id
+    });
+    this.processQueue();
+    return this;
+  };
 
-		this.callbacks = {};
-	  this.filename = filename;
-	  this.count = count || 4;
-	  this.queue = [];
-	  this.results = [];
-	  this.pool = [];
-	  this.working = {};
-	  this.uuid = 0;
-	  return this;
+  labor.Pool.prototype.processQueue = function() {
+    if (this.queue.length == 0 && this.pool.length == this.count) {
+      if (this.onfinish) this.onfinish();
+    } else {
+      while (this.queue.length > 0 && this.pool.length > 0) {
+        var unit = this.queue.shift();
+        var worker = this.pool.shift();
+        worker.id = unit.id;
+        this.working[worker.id] = worker;
+        var self = this;
 
-	};
+        // we tell the worker what to do upon completing its task
+        worker.onmessage = function(e) {
+          var id = this.id;
+          delete self.working[this.id];
+          this.id = null;
+          self.pool.push(this);
 
-	labor.Pool.prototype.start = function() {
-		this.fillPool();
-		return this;
-	}
+          try {
+            if (self.callbacks[id]) {
+              self.callbacks[id](e.data.result);
+              delete self.callbacks[id];
+            }
+          } catch (error) {
+            console.warn(error);
+          }
 
-	labor.Pool.prototype.onfinish = function() {};
+          self.processQueue();
+        };
 
-	labor.Pool.prototype.addWork = function( function_name, arguments_array, callback ) {
+        worker.postMessage(unit);
+      }
+    }
+    return this;
+  };
 
-	  var id = this.uuid++;
-	  this.callbacks[ id ] = callback;
-	  this.queue.push( { func: function_name, arguments: arguments_array, 'id': id });
-	  this.processQueue();
-	  return this;
+  labor.Pool.prototype.addWorker = function() {
+    var w = new Worker(this.filename);
+    var self = this;
 
-	};
+    this.pool.push(w);
+    return this;
+  };
 
-	labor.Pool.prototype.processQueue = function() {
-
-	  if (this.queue.length == 0 && this.pool.length == this.count) {
-
-	    if (this.onfinish)
-	      this.onfinish();
-
-	  } else {
-	    while (this.queue.length > 0 && this.pool.length > 0) {
-
-	      var unit = this.queue.shift();
-	      var worker = this.pool.shift();
-	      worker.id = unit.id;
-	      this.working[worker.id] = worker;
-	      var self = this;
-
-	      // we tell the worker what to do upon completing its task
-	      worker.onmessage = function( e ){
-		
-				var id = this.id;
-			    delete self.working[this.id];
-			    this.id = null;
-			    self.pool.push(this); 
-
-			    try { 
-
-			      if ( self.callbacks[ id ] )
-						{
-							self.callbacks[ id ]( e.data.result );
-							delete self.callbacks[id];
-						}
-
-			    } catch(error) {
-			      console.warn(error);
-			    }
-
-			    self.processQueue();
-
-				};
-
-	      worker.postMessage( unit ); 
-
-	    }
-	  }
-	  return this;
-	};
-
-	labor.Pool.prototype.addWorker = function() {
-
-	  var w = new Worker(this.filename);
-	  var self = this;
-
-	  this.pool.push(w);
-	  return this;
-
-	};
-
-	labor.Pool.prototype.fillPool = function() {
-	  for (var i=0; i<this.count; i++) {
-	    this.addWorker();
-	  }
-	};
-
+  labor.Pool.prototype.fillPool = function() {
+    for (var i = 0; i < this.count; i++) {
+      this.addWorker();
+    }
+  };
 })(labor);
-
